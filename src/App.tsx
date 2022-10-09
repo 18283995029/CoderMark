f72d4052baee06280524ed3f834bb464167e74c5
sun_wang <sun_wang@medinno.onmicrosoft.com>
2022-10-09 17:44:14 +0800

    1744

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <h2>CoderMark</h2>
     </div>
   );
